c7344d6b09d46ec6110a4774dff04e1e75089b8e
Joshua Rymer <joshuarymer1@gmail.com>
2022-10-03 12:34:33 +0100

    Add basic round count functionality

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ btn3.addEventListener("click", () => playRound("scissors", getComputerChoice()))
 
 const results = document.getElementById("resultscon");
 
-
-
-
+let currentRound = 1;
+let playerScore = 0;
+let computerScore = 0;
 function getComputerChoice() {
         let randomNum = Math.floor((Math.random()*3));
         if (randomNum === 0) {
@@ -26,31 +26,56 @@ function getComputerChoice() {
  
 function playRound(playerInput, computerSelection) {
     if (playerInput === "rock" && computerSelection === "paper") {
-        results.textContent = "You lose. Paper covers rock.";
+        computerScore += 1;
+        results.textContent += "Round " + currentRound + ": You lose. Paper covers rock.\nYou: "
+                             + playerScore + " Computer: " + computerScore + ".\n";
+        currentRound += 1;
     }
     else if(playerInput === "rock" && computerSelection === "scissors") {
-        results.textContent = "You win. Rock blunts scissors.";
+        playerScore += 1;
+        results.textContent += "Round " + currentRound + ": You win. Rock blunts scissors.\nYou: "
+                            + playerScore + " Computer: " + computerScore + ".\n";
+        currentRound += 1;
     }
     else if(playerInput === "rock" && computerSelection === "rock") {
-        results.textContent = "Draw.";
+        results.textContent += "Round " + currentRound + ": Draw.\nYou: "
+                            + playerScore + " Computer: " + computerScore + ".\n";
+        currentRound += 1;
     }    
     else if (playerInput === "scissors" && computerSelection === "paper") {
-        results.textContent = "You win. Scissors cut paper.";
+        playerScore += 1;
+        results.textContent += "Round " + currentRound + ": You win. Scissors cut paper.\nYou: "
+                            + playerScore + " Computer: " + computerScore + ".\n";
+        currentRound += 1;
     }
     else if(playerInput === "scissors" && computerSelection === "scissors") {
-        results.textContent = "Draw.";
+        results.textContent += "Round " + currentRound + ": Draw.\nYou: "
+                            + playerScore + " Computer: " + computerScore + ".\n";
+        currentRound += 1;
     }
     else if(playerInput === "scissors" && computerSelection === "rock") {
-        results.textContent = "You lose. Rock blunts scissors.";
+        computerScore += 1;
+        results.textContent += "Round " + currentRound + ": You lose. Rock blunts scissors.\nYou: "
+                            + playerScore + " Computer: " + computerScore + ".\n";
+        
+        currentRound += 1;
     }
     else if (playerInput === "paper" && computerSelection === "paper") {
-        results.textContent = "Draw.";
+        results.textContent += "Round " + currentRound + ": Draw.\nYou: "
+                            + playerScore + " Computer: " + computerScore + ".\n";
+        currentRound += 1;
     }
     else if(playerInput === "paper" && computerSelection === "scissors") {
-        results.textContent = "You lose. Scissors cut paper.";
+        computerScore += 1;
+        results.textContent += "Round " + currentRound + ": You lose. Scissors cut paper.\nYou: "
+                            + playerScore + " Computer: " + computerScore + ".\n";
+        currentRound += 1;
     }
     else if(playerInput === "paper" && computerSelection === "rock") {
-        results.textContent = "You win. Paper covers rock.";
+        playerScore += 1;
+        results.textContent += "Round " + currentRound + ": You win. Paper covers rock.\nYou: "
+                            + playerScore + " Computer: " + computerScore + ".\n";
+        currentRound += 1;
     }
 }
 
